6692922ffa7a0941212f4ac895715c58d1cdd35e
ontama154 <onchanvvv@gmail.com>
2022-04-11 09:19:42 +0900

    Initial Commit

---
+++ UIKit+MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit+MVVM
+//
+//  Created by tomoyo_kageyama on 2022/04/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
